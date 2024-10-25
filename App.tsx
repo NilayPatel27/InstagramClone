93215bdca6b97a502d183ed0b62c06cfd76a9a99
Nilay Patel <nilaykumarkanubhai.patel@ontariotechu.net>
2024-10-25 00:53:05 -0400

    Resolve project start up issue

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import RootNavigation from './src/navigation/rootNavigation'
-import Login from './src/auth/Login/LoginPage'
 
 const App = () => {
   return (
-    <View>
-      {/* <RootNavigation /> */}
-      <Login />
-    </View>
+      <RootNavigation />
   )
 }
 
