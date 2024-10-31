cf8b4d53914d0ed8a95833cf793096b3894af260
Prerna Gupta <prerna.gupta@ontariotechu.net>
2024-10-31 01:14:06 -0400

    Signup page preparation......
    Authentication preparation

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 5000
+const {MONGOURI} = require('./keys')
+
+require('./models/user')
+
+app.use(require('./routes/auth'))
+
+
+mongoose.connect(MONGOURI)
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongo..")
+})
+    mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+app.listen(PORT,()=>{
+    console.log("sever is running on",PORT)
+})
