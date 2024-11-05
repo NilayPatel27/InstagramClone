0d9d94f34e37e3bbcdfa07c2a439e4e605e7a38c
Prince Patel <prince90290@gmail.com>
2024-11-05 18:08:47 -0500

    Implemented Login functionality-api in NodeJs

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const app = express()
 const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI} = require('./keys')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('./models/User')
 
 require('./models/user')
 
 app.use(require('./routes/auth'))
-
+app.use(express.json());
 
 mongoose.connect(MONGOURI)
 mongoose.connection.on('connected',()=>{
@@ -16,6 +19,33 @@ mongoose.connection.on('connected',()=>{
     mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
 })
+
+app.post('/api/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign(
+            { userId: user._id },
+            'secret_key',
+            { expiresIn: '1h' }
+        );
+
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
 
 app.listen(PORT,()=>{
     console.log("sever is running on",PORT)
