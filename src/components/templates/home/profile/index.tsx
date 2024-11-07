84ff648c1b4c0aaee5e8b6e79fecffe19f79d347
Prerna Gupta <prerna.gupta@ontariotechu.net>
2024-11-06 23:14:56 -0500

    Updated the description for profile in React Native

--- src/components/templates/home/profile/index.tsx
+++ src/components/templates/home/profile/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, FlatList, Image, StyleSheet, Text, View } from 'react-native';
 
 import { Images } from '@instagram/assets';
 import { NavigationBar } from '@instagram/components/molecules/index.tsx';
+import { Divider } from '@rneui/base';
 
 const ProfileTemplate = () => {
 
@@ -14,6 +15,23 @@ const ProfileTemplate = () => {
     }
 
     const description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+    //Dummy data for posts
+    const data = [
+        { id: 1, image: Images.Back },
+        { id: 2, image: Images.Remove },
+        { id: 3, image: Images.Edit },
+        { id: 4, image: Images.Image },
+        { id: 5, image: Images.Plus },
+        { id: 6, image: Images.WhitePlus },
+        { id: 7, image: Images.Delete },
+        { id: 8, image: Images.Hide },
+        { id: 9, image: Images.HideAll },
+        { id: 10, image: Images.Report },
+    ];
+
+    const screenWidth = Dimensions.get('window').width;
+    const postSize = screenWidth / 3 - 2;
     return (
         <>
             <NavigationBar rightProps={{ onPress, back: false, right: true, onBack: false, postButton: true }} navigation={navigation} />
@@ -42,6 +60,23 @@ const ProfileTemplate = () => {
                 <Text style={styles.username}>User Name</Text>
                 <Text style={styles.description}>{description}</Text>
             </View>
+            <Divider />
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={{
+                        margin: 1,
+                        width: postSize,
+                        height: postSize,
+                    }}>
+                        <Image source={item.image} style={styles.postImage} />
+                    </View>
+                )}
+                numColumns={3}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ backgroundColor: 'white' }}
+            />
         </>
     )
 }
@@ -85,11 +120,6 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
         color: 'black',
-    },
-    postContainer: {
-        flex: 1,
-        aspectRatio: 1,
-        margin: 2,
     },
     postImage: {
         width: '100%',
