dd44b89b47d86a8e9788db24b7f61d2962e5ad60
Nilay Patel <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-09 00:36:53 -0500

    Implemented common api handling in React Native

---
+++ src/context/appContext/apiManagement/apiConfig.tsx
@@ -0,0 +1,4 @@
+export const MULTI_PART_HEADER = {
+    "content-type": "multipart/form-data"
+};
+export const HEADER = { "Content-Type": "application/json" };
---
+++ src/context/appContext/apiManagement/index.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { HEADER, MULTI_PART_HEADER } from "./apiConfig";
+
+const GET = async (url: any, header: any) => {
+    const newHeader = { ...HEADER, ...header };
+    try {
+        const response = await axios.get(url, newHeader);
+        __DEV__ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n HEADER:", header);
+        return response;
+    } catch (error) {
+        console.log("Error fetching data", error);
+    };
+}
+
+const POST = async (url: any, data: any, header: any) => {
+    const newHeader = { ...HEADER, ...header };
+    try {
+        const response = await axios.post(url, data, newHeader);
+        __DEV__ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n PARAMS:", data, "\n \n HEADER:", newHeader);
+        return response;
+    } catch (error) {
+        console.log("Error posting data", error);
+    };
+}
+
+const FORMDATA_POST = async (url: any, data: any, header: any) => {
+    const newHeader = { ...MULTI_PART_HEADER, ...header };
+    try {
+        const response = await axios.post(url, data, newHeader);
+        __DEV__ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n PARAMS:", data, "\n \n HEADER:", newHeader);
+        return response;
+    } catch (error) {
+        console.log("Error posting data", error);
+    };
+}
+
+export { GET, POST, FORMDATA_POST };
