9c9e63a5462d610b5805e875cdf515ebf61141fe
Nilay Patel <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-09 00:37:15 -0500

    Handled logout request in React Native

--- src/context/appContext/action/auth/index.tsx
+++ src/context/appContext/action/auth/index.tsx
@@ -1,14 +1,30 @@
+import config from "react-native-config";
+
 import TYPES from "@instagram/context/appContext/types";
 import { useAccess } from "@instagram/customHooks/useAccess";
+import { GET, POST } from "@instagram/context/appContext/apiManagement/index.tsx";
 
-export const loginRequest = (dispatch: any) => async (response: any) => {
+const { ACCESS_KEY, LOGINURL } = config;
+
+export const loginRequest = (dispatch: any) => async (params: any) => {
     try {
-        const newAuthState = await response.json();
-        await useAccess("instagram", JSON.stringify({
-            accessToken: newAuthState?.accessToken
+        const response = await POST(LOGINURL, params, {});
+        await useAccess(ACCESS_KEY, JSON.stringify({
+            accessToken: response?.accessToken
         }));
-        dispatch({ type: TYPES.LOGIN_SUCCESS, payload: newAuthState });
+        dispatch({ type: TYPES.LOGIN_SUCCESS, payload: response });
     } catch (error) {
         dispatch({ type: TYPES.LOGIN_FAILURE, payload: error });
+    }
+};
+
+export const logoutRequest = (dispatch: any) => async () => {
+    try {
+        const response = await GET(LOGINURL, {});
+        __DEV__ && console.log("LogOut", response);
+        await useAccess(ACCESS_KEY, "");
+        dispatch({ type: TYPES.LOGOUT_SUCCESS, payload: {} });
+    } catch (error) {
+        dispatch({ type: TYPES.LOGOUT_FAILURE, payload: error });
     }
 };
