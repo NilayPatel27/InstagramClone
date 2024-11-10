3104695c85e9857896f75a3fb0f7fb56cbb9f10e
Nilay Patel <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-10 09:16:21 -0500

    Handled user exists api in Node js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post('/signup', (req, res) => {
     const { name, email, password } = req.body
@@ -36,4 +37,23 @@ router.post('/signup', (req, res) => {
             console.log(err)
         })
 })
+
+//username exist
+router.post('/usernameexist', (req, res) => {
+    const { name } = req.body
+    if (!name) {
+        return res.status(422).json({ error: "Please add all the fields" })
+    }
+    User.findOne({ name: name })
+        .then((savedUser) => {
+            if (savedUser) {
+                return res.status(422).json({ error: "user already exists with that name" })
+            }
+            res.json({ message: "user not exist" })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
 module.exports = router
