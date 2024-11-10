13f16d119cd33c4367540c3fefa5be00455a72f3
Nilay Patel <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-10 10:52:58 -0500

    Transfer login api code to main page in Node js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -56,4 +56,34 @@ router.post('/usernameexist', (req, res) => {
         })
 })
 
+router.post('/login', (req, res) => {
+    const { email, password } = req.body
+    console.log({ email, password })
+    if (!email || !password) {
+        return res.status(422).json({ error: "Please add email or password" })
+    }
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({ error: "Invalid Email or password" })
+            }
+            console.log({ savedUser })
+            bcrypt.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        const token = jwt.sign(
+                            { userId: savedUser._id.toString() },
+                            'Instagram@123'
+                        )
+                        const { _id, name, email } = savedUser;
+                        res.json({ token, user: { _id, name, email }, message: "Successfully signed in" });
+                    } else {
+                        return res.status(422).json({ error: "Invalid Email or password" })
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+})
 module.exports = router
