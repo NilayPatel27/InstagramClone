6cd36fba0da8c89f942df2120b77a487ccfc08b1
Nilay Patel <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-10 21:25:32 -0500

    Created custom hook to get local storage data in React Native

--- src/customHooks/useAccess/index.tsx
+++ src/customHooks/useAccess/index.tsx
@@ -7,3 +7,12 @@ export const useAccess = async (key: any, value: any) => {
         console.log("Error saving data", error);
     };
 }
+
+export const useGetAccess = async (key: any) => {
+    try {
+        const value = await AsyncStorage.getItem(key);
+        return value;
+    } catch (error) {
+        console.log("Error fetching data", error);
+    };
+}
