490a35098afb1b8b63b83b921d412bdc58dcf30d
Prince Patel <prince90290.dev@gmail.com>
2024-11-11 20:02:14 -0500

    Handled Delete API in React Native

--- src/context/appContext/apiManagement/index.tsx
+++ src/context/appContext/apiManagement/index.tsx
@@ -5,7 +5,7 @@ const GET = async (url: any, header: any) => {
     const newHeader = { ...HEADER, ...header };
     try {
         const response = await axios.get(url, newHeader);
-        __DEV__ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n HEADER:", header);
+        _DEV_ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n HEADER:", header);
         return response;
     } catch (error) {
         console.log("Error fetching data", error);
@@ -18,7 +18,7 @@ const POST = async (url: any, data: any, header: any) => {
         console.log(url, data, { headers: newHeader });
         // const response = await axios.post(url, data, { headers: newHeader });
         const response = await axios.post(url, data, newHeader);
-        __DEV__ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n PARAMS:", data, "\n \n HEADER:", newHeader);
+        _DEV_ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n PARAMS:", data, "\n \n HEADER:", newHeader);
         return response;
     } catch (error) {
         console.log("Error posting data", error);
@@ -29,11 +29,22 @@ const FORMDATA_POST = async (url: any, data: any, header: any) => {
     const newHeader = { ...MULTI_PART_HEADER, ...header };
     try {
         const response = await axios.post(url, data, { headers: newHeader });
-        __DEV__ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n PARAMS:", data, "\n \n HEADER:", newHeader);
+        _DEV_ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n PARAMS:", data, "\n \n HEADER:", newHeader);
         return response;
     } catch (error) {
         console.log("Error posting data", error);
     };
 }
 
-export { GET, POST, FORMDATA_POST };
+const DELETE = async (url: any) => {
+    try {
+        console.log("deleteUSerUrl", url);
+        const response = await axios.delete(url);
+        _DEV_ && console.log("Result:  ", response, "\n \n URL:", url, "\n \n HEADER:");
+        return response;
+    } catch (error) {
+        console.log("Error fetching data", error);
+    };
+}
+
+export { GET, POST, FORMDATA_POST, DELETE };
