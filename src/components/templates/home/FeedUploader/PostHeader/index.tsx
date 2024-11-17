91e6e75674da8b689d7b1335dea96ec6fe629ddd
Prince Patel <prince90290.dev@gmail.com>
2024-11-17 18:51:48 -0500

    Added OneFeed page Template in React Native

--- src/components/templates/home/FeedUploader/PostHeader/index.tsx
+++ src/components/templates/home/FeedUploader/PostHeader/index.tsx
@@ -1,7 +1,7 @@
-import { Image, StyleSheet, Text, useWindowDimensions, View } from 'react-native'
+import { Image, StyleSheet, Text, Touchable, TouchableOpacity, useWindowDimensions, View } from 'react-native'
 import React from 'react'
 import { Images } from "@instagram/assets/index.tsx";
-
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 interface PostHeaderProps {
     userName: string,
     profileUri: string,
@@ -13,13 +13,21 @@ const PostHeader = ({ userName, profileUri }: PostHeaderProps) => {
     return (
         <View style={{
             flexDirection: "row", justifyContent: "space-between", backgroundColor: "#fff",
-            width: windowWidth, alignItems: "center", borderBottomWidth: 0.5, paddingVertical: 5
+            width: windowWidth, alignItems: "center", borderBottomWidth: 0.5, padding: 10
         }}>
             <View style={{ alignItems: "center", flexDirection: "row" }}>
                 {profileUri && profileUri !== null ? <Image source={{ uri: `${profileUri}` }} style={{ height: 25, marginHorizontal: 10, width: 25 }} /> :
                     <Image source={Images.Plus} style={{ height: 25, marginHorizontal: 10, width: 25 }} />}
-                <Text style={{ color: "rgb(0, 0, 0)", fontSize: 15, fontWeight: "bold" }}>{userName ? userName.toUpperCase() : "userName"}</Text>
+                <Text style={{ color: "rgb(0, 0, 0)", fontSize: 15, fontWeight: "bold" }}>{userName}</Text>
             </View>
+            <TouchableOpacity
+                onPress={() => { }}>
+                <SimpleLineIcons
+                    name="options-vertical"
+                    size={20}
+                    color={'black'}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
--- src/components/templates/home/FeedsList/OneFeed/index.tsx
+++ src/components/templates/home/FeedsList/OneFeed/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, useWindowDimensions, Image } from 'react-native';
 
-const OneFeedTemplate = () => {
+import { useUserData } from '@instagram/customHooks';
+import PostHeader from '@instagram/components/templates/home/FeedUploader/PostHeader/index';
+
+const OneFeedTemplate = ({ image }: any) => {
+    const { width: windowWidth } = useWindowDimensions();
+
+    const { userData } = useUserData();
     return (
-        <View>
-            <Text>OneFeedTemplate</Text>
-        </View>
+        <>
+            <PostHeader userName={userData?.user?.name ? userData?.user?.name : "User Name"} profileUri={""} options={false} />
+            <View style={{
+                justifyContent: "center", alignItems: "center", width: "100%", backgroundColor: "white", marginBottom: 10
+            }}>
+                <Image
+                    source={{ uri: image }}
+                    resizeMode="contain"
+                    style={{ width: windowWidth, aspectRatio: 1, marginVertical: 5 }}
+                />
+            </View>
+        </>
     )
 }
 
