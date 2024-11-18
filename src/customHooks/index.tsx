720c9f9d37ee7c91ef0d287f183d9e3843c5708f
Nilay Patel <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-18 00:48:42 -0500

    Created Custom hook for delete Feed api in React Native

--- src/customHooks/index.tsx
+++ src/customHooks/index.tsx
@@ -1,5 +1,6 @@
 import useStoragePermission from './useStoragePermission';
 import usePrevious from './usePrevious';
 import useUserData from './useUserData';
+import useDeleteFeed from './useDeleteFeed';
 
-export { useStoragePermission, usePrevious, useUserData };
+export { useStoragePermission, usePrevious, useUserData, useDeleteFeed };
---
+++ src/customHooks/useDeleteFeed/index.tsx
@@ -0,0 +1,56 @@
+import { Alert } from "react-native";
+import { useContext, useEffect, useState } from "react";
+
+import { AppContext } from "@instagram/context";
+import usePrevious from "@instagram/customHooks/usePrevious";
+
+const useDeleteFeed = () => {
+
+    const { state: AppState, deleteUserFeedRequest } = useContext(AppContext);
+
+    const previousAppState: any = usePrevious(AppState);
+
+    const [deleteUserFeedLoading, setDeleteUserFeedLoading] = useState(false);
+
+    useEffect(() => {
+        if (deleteUserFeedLoading && AppState?.Auth?.deleteUserFeedSuccess === true && AppState?.Auth?.deleteUserFeedResponse) {
+            if (previousAppState?.Auth !== AppState?.Auth) {
+                setDeleteUserFeedLoading(false);
+                if (AppState?.Auth?.deleteUserFeedResponse?.status === "Success" || AppState?.Auth?.deleteUserFeedResponse?.status === 200) {
+
+                } else {
+                    Alert.alert(
+                        "Alert",
+                        AppState?.Auth?.deleteUserFeedResponse?.message ? AppState?.Auth?.deleteUserFeedResponse?.message : "Something went wrong",
+                        [
+                            {
+                                text: "OK",
+                                onPress: () => { }
+                            }
+                        ],
+                        { cancelable: false }
+                    );
+                }
+            }
+        } else if (deleteUserFeedLoading && AppState?.Auth && AppState?.Auth?.deleteUserFeedSuccess === false && AppState?.Auth?.error) {
+            if (previousAppState?.Auth !== AppState?.Auth) {
+                setDeleteUserFeedLoading(false);
+                if (AppState?.Auth?.error && AppState?.Auth?.error?.code && AppState?.Auth?.error?.code === 401) {
+                    Alert.alert("", AppState?.Auth?.error?.error?.toString());
+                } else {
+                    Alert.alert(AppState?.Auth?.error?.error)
+                }
+            }
+        }
+    }, [deleteUserFeedLoading, AppState?.Auth?.deleteUserFeedSuccess, AppState?.Auth?.deleteUserFeedResponse, AppState?.Auth?.error]);
+
+
+    const deleteFeed = ({ feedId, userId }: any) => {
+        setDeleteUserFeedLoading(true);
+        deleteUserFeedRequest({ feedId, userId });
+    }
+
+    return { deleteFeed, deleteUserFeedLoading };
+};
+
+export default useDeleteFeed;
