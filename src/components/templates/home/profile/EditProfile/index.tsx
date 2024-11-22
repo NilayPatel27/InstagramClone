030e315e09ed1d461d34a10934586187ec41db30
NilayPatel27 <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-22 10:12:30 -0500

    Added User Image in Edit Profile Page in React Native

--- src/components/templates/home/profile/EditProfile/index.tsx
+++ src/components/templates/home/profile/EditProfile/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
+import { Images } from '@instagram/assets';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationBar } from '@instagram/components/molecules';
+import { useUserData } from '@instagram/customHooks';
 
 const EditPorfileTemplate = () => {
 
@@ -11,12 +13,34 @@ const EditPorfileTemplate = () => {
     const onBack = () => {
         navigation.goBack()
     }
+    const { userData } = useUserData();
 
     return (
         <View style={{ flex: 1, backgroundColor: "white" }}>
             <NavigationBar rightProps={{ back: true, right: false, onBack, text: "Edit Profile" }} navigation={navigation} />
+            <View style={styles.imageContainer}>
+                {
+                    userData?.user?.profileImage ?
+                        <Image source={{ uri: userData?.user?.profileImage }} style={styles.userImage} /> :
+                        <Image source={Images.User} style={styles.userImage} />
+                }
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    userImage: {
+        width: 80,
+        height: 80,
+        borderRadius: 40
+    }
+});
 
 export default EditPorfileTemplate
