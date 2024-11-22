773f5f8e01251b1c6187de90b71f77849e7a197d
prince02765 <princepatel221350@gmail.com>
2024-11-22 19:27:00 +0000

    Added Search bar in Search Page in React Native

--- src/components/templates/home/search/index.tsx
+++ src/components/templates/home/search/index.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
 
 const SearchTemplate = () => {
+
+    const [searchText, setSearchText] = useState("");
+
     return (
-        <View>
-            <Text>SearchTemplate</Text>
+        <View style={{ height: '100%', width: "100%", backgroundColor: "white" }}>
+            <View style={styles.searchBarContainer}>
+                <TextInput
+                    placeholder="Search"
+                    style={{ padding: 5, marginLeft: 10, color: "black" }}
+                    value={searchText}
+                    onChangeText={(text) => setSearchText(text)}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    searchBarContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 5,
+        backgroundColor: 'lightgray',
+        borderRadius: 30,
+        margin: 10
+    }
+});
 
 export default SearchTemplate
