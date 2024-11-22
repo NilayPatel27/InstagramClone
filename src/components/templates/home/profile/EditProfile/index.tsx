8103091dda911e434db2acc57e5d917a3f89a40b
NilayPatel27 <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-22 12:19:02 -0500

    Added Image picker functionality in Edit profile page in React Native

--- src/components/templates/home/profile/EditProfile/index.tsx
+++ src/components/templates/home/profile/EditProfile/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import DocumentPicker from "react-native-document-picker";
+import { Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { Images } from '@instagram/assets';
-import { useUserData } from '@instagram/customHooks';
 import { NavigationBar } from '@instagram/components/molecules';
+import { useStoragePermission, useUserData } from '@instagram/customHooks';
 
 const EditPorfileTemplate = () => {
 
@@ -13,13 +14,72 @@ const EditPorfileTemplate = () => {
     const onBack = () => {
         navigation.goBack()
     }
+
     const { userData } = useUserData();
 
+    const [userProfileImage, setUserProfileImage] = useState(userData?.user?.profileImage || "");
     const [userFullName, setUserFullName] = useState(userData?.user?.name || "User Name");
     const [userBio, setUserBio] = useState(userData?.user?.bio || "Bio");
 
-    const onEditPicturePress = () => {
+    const [images, setImages] = useState("");
+
+    const { permission, requestStoragePermission } = useStoragePermission();
+
+    const onEditPicturePress = async () => {
+
+        if (permission === 'never_ask_again') {
+            console.log("Image Picker");
+            const file: any = await DocumentPicker.pick({
+                allowMultiSelection: false,
+                type: [DocumentPicker.types.images],
+                presentationStyle: "fullScreen"
+            }).catch(() => {
+                goBack();
+            });
+
+            let valid = true;
+
+            file?.map((item: any) => {
+                if (["jpeg", "jpg", "png", "gif"].indexOf(item?.name?.split(".").pop()?.toLowerCase()) === -1 || item?.size > 150000) {
+                    return valid = false;
+                }
+            });
+
+            if (valid) {
+                setImages(file);
+                setUserProfileImage(file[0]?.uri);
+            } else {
+                Alert.alert("", "Upload .jpeg, .jpg, .png or .gif images only and size should be less than 150 kb", [
+                    { text: "OK", onPress: () => { } }
+                ]);
+            }
+
+        } else {
+            Alert.alert("", "Please allow access to storage in app settings", [
+                { text: "OK", onPress: () => { } }
+            ]);
+        }
     }
+
+    useEffect(() => {
+        requestStoragePermission();
+    }, []);
+
+    const goBack = () => {
+        if (images?.length > 0) {
+            Alert.alert("", "Are you sure you want to discard your images?", [
+                { text: "No", onPress: () => { } },
+                {
+                    text: "Yes", onPress: () => {
+                        setImages("");
+                    }
+                },
+            ]);
+        }
+        else {
+            navigation.goBack();
+        }
+    };
 
     const onUploadButtonPress = () => {
     }
@@ -29,8 +89,8 @@ const EditPorfileTemplate = () => {
             <NavigationBar rightProps={{ back: true, right: true, onBack, text: "Edit profile", onPress: onUploadButtonPress, uploadButton: true }} navigation={navigation} />
             <View style={styles.imageContainer}>
                 {
-                    userData?.user?.profileImage ?
-                        <Image source={{ uri: userData?.user?.profileImage }} style={styles.userImage} /> :
+                    userProfileImage ?
+                        <Image source={{ uri: userProfileImage }} style={styles.userImage} /> :
                         <Image source={Images.User} style={styles.userImage} />
                 }
             </View>
