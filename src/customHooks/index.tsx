769c79d1d46c166d416d8af3eaf7c03d58197762
NilayPatel27 <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-22 23:56:12 -0500

    Created custom hook for alluserlist api in React Native

--- src/customHooks/index.tsx
+++ src/customHooks/index.tsx
@@ -3,5 +3,6 @@ import useUserData from './useUserData';
 import useFeedsList from './useFeedsList';
 import useDeleteFeed from './useDeleteFeed';
 import useStoragePermission from './useStoragePermission';
+import useAllUsersList from './useAllUsersList';
 
-export { useStoragePermission, usePrevious, useUserData, useDeleteFeed, useFeedsList };
+export { useStoragePermission, usePrevious, useUserData, useDeleteFeed, useFeedsList, useAllUsersList };
---
+++ src/customHooks/useAllUsersList/index.tsx
@@ -0,0 +1,58 @@
+import { Alert } from "react-native";
+import { useContext, useEffect, useState } from "react";
+
+import { AppContext } from "@instagram/context";
+import usePrevious from "@instagram/customHooks/usePrevious";
+
+const useAllUsersList = () => {
+
+    const { state: AppState, allUsersRequest } = useContext(AppContext);
+
+    const previousAppState: any = usePrevious(AppState);
+
+    const [allUsersListLoading, setAllUsersListLoading] = useState(false);
+
+    const [allUsersList, setAllUsersList] = useState([]);
+
+    useEffect(() => {
+        if (allUsersListLoading && AppState?.Auth?.allUsersSuccess === true && AppState?.Auth?.allUsersResponse) {
+            if (previousAppState?.Auth !== AppState?.Auth) {
+                setAllUsersListLoading(false);
+                if (AppState?.Auth?.allUsersResponse?.status === "Success" || AppState?.Auth?.allUsersResponse?.status === 200) {
+                    setAllUsersList(AppState?.Auth?.allUsersResponse?.data);
+                } else {
+                    Alert.alert(
+                        "Alert",
+                        AppState?.Auth?.allUsersResponse?.message ? AppState?.Auth?.allUsersResponse?.message : "Something went wrong",
+                        [
+                            {
+                                text: "OK",
+                                onPress: () => { }
+                            }
+                        ],
+                        { cancelable: false }
+                    );
+                }
+            }
+        } else if (allUsersListLoading && AppState?.Auth && AppState?.Auth?.allUsersSuccess === false && AppState?.Auth?.error) {
+            if (previousAppState?.Auth !== AppState?.Auth) {
+                setAllUsersListLoading(false);
+                if (AppState?.Auth?.error && AppState?.Auth?.error?.code && AppState?.Auth?.error?.code === 401) {
+                    Alert.alert("", AppState?.Auth?.error?.error?.toString());
+                } else {
+                    Alert.alert(AppState?.Auth?.error?.error)
+                }
+            }
+        }
+    }, [allUsersListLoading, AppState?.Auth?.allUsersSuccess, AppState?.Auth?.allUsersResponse, AppState?.Auth?.error]);
+
+
+    const getAllUsersList = () => {
+        setAllUsersListLoading(true);
+        allUsersRequest();
+    }
+
+    return { allUsersList, getAllUsersList, allUsersListLoading };
+};
+
+export default useAllUsersList;
