bb8dfc1cf3217d7ec2fb422b53ef4da48438ba0b
prernagupta2226 <prerna.gupta@ontariotechu.net>
2024-11-23 03:02:45 -0500

    Created the follow and unfollow api users

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
-
-
-
-
-// Delete a post by postID
 router.delete('/delete/:postId', async (req, res) => {
   try {
     const { postId } = req.params;
