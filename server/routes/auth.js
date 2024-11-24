e2ca25b1dda5fe0484e52f91bd721737c2cd8e0b
prince02765 <princepatel221350@gmail.com>
2024-11-24 18:52:16 +0000

    Created getUserDetails api in Node js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -214,4 +214,19 @@ router.get('/alluserslist', upload.none(), async (req, res) => {
     }
 
 });
+
+//getUserDetails 
+router.get('/user/:userId', async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+        res.status(200).json({ user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to get user details.' });
+    }
+});
 module.exports = router
