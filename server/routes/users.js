4c86f56ba9a9878942ec2690476e10a750d32fc4
Prerna Gupta <prerna.gupta@ontariotechu.net>
2024-11-25 15:46:59 +0000

    Removed follow-unfollow api code from the users api handler page in Node js

--- server/routes/users.js
+++ server/routes/users.js
@@ -21,48 +21,6 @@ router.get('/user/:id', (req, res) => {
     })
 })
 
-router.put('/follow', (req, res) => {
-  User.findByIdAndUpdate(req.body.followId, {
-    $push: { followers: req.user._id }
-  }, {
-    new: true
-  }, (err, result) => {
-    if (err) {
-      return res.status(422).json({ error: err })
-    }
-    User.findByIdAndUpdate(req.user._id, {
-      $push: { following: req.body.followId }
-
-    }, { new: true }).then(result => {
-      res.json(result)
-    }).catch(err => {
-      return res.status(422).json({ error: err })
-    })
-
-  }
-  )
-
-})
-router.put('/unfollow', (req, res) => {
-  User.findByIdAndUpdate(req.body.unfollowId, {
-    $pull: { followers: req.user._id }
-  }, {
-    new: true
-  }, (err, result) => {
-    if (err) {
-      return res.status(422).json({ error: err })
-    }
-    User.findByIdAndUpdate(req.user._id, {
-      $pull: { following: req.body.unfollowId }
-
-    }, { new: true }).then(result => {
-      res.json(result)
-    }).catch(err => {
-      return res.status(422).json({ error: err })
-    })
-  }
-  )
-})
 router.get('/search', async (req, res) => {
   try {
     const { query } = req.query;
