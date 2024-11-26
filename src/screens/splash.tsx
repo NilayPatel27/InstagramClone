80738cc3f43da509a322390b2bd47cc185379195
Prernagupta2226 <prerna.gupta@ontariotechu.net>
2024-11-26 12:27:27 -0500

    Implemented Splash screen page in React Native

--- src/screens/splash.tsx
+++ src/screens/splash.tsx
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from 'react-native';
+import React, { useEffect } from 'react';
+import { CommonActions, useNavigation } from '@react-navigation/native';
+
+import { AppLogo } from '@instagram/components/molecules';
+import { getAccess } from '@instagram/customHooks/useAccess';
 
 const Splash = () => {
+
+    const navigation = useNavigation();
+
+    const checkAuthStatus = async () => {
+
+        const authToken = await getAccess("userToken");
+
+        setTimeout(() => {
+            navigation.dispatch(
+                CommonActions.reset({
+                    index: 0,
+                    routes: [
+                        {
+                            name: authToken ? "HomeStack" : "AuthStack",
+                        }
+                    ],
+                })
+            );
+        }, 1000);
+    };
+
+    useEffect(() => {
+        checkAuthStatus();
+    }, []);
+
     return (
-        <View>
-            <Text>Splash</Text>
+        <View style={{
+            alignItems: "center",
+            flex: 1,
+            justifyContent: "center",
+            padding: 10,
+            backgroundColor: "white"
+        }}>
+            <AppLogo />
         </View>
     )
 }
 
-export default Splash
+export default Splash;
