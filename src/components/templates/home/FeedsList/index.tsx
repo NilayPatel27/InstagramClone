feed148cadef9acf827b855799ff1c0a04f0a910
NilayPatel27 <nilaykumarkanubhai.patel@ontariotechu.net>
2024-11-27 01:02:40 -0500

    Added NoPostsImage in FeedsList page in React Native

--- src/components/templates/home/FeedsList/index.tsx
+++ src/components/templates/home/FeedsList/index.tsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList, Image, StyleSheet } from 'react-native';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useNavigation, useNavigationState } from '@react-navigation/native';
+import { View, FlatList, Image, StyleSheet, Text, Dimensions } from 'react-native';
 
 import { Images } from '@instagram/assets';
 import { Loader } from '@instagram/components/atoms';
+import { NavigationBar } from '@instagram/components/molecules';
 import { useDeleteFeed, useFeedsList, useUserData } from '@instagram/customHooks';
 import { OneFeedTemplate, MultiFeedsTemplate } from '@instagram/components/templates/home/index.tsx';
-import { NavigationBar } from '@instagram/components/molecules';
 
 const FeedsListTemplate = ({ otherUserId }: { otherUserId: string }) => {
 
+    const { width, height } = Dimensions.get('window');
+
     const { deleteFeed, deleteUserFeedLoading, deletFeedSuccess } = useDeleteFeed();
 
-    const { getFeedsList, userFeedListLoading, userFeedList: userFeedsList } = useFeedsList();
+    const { getFeedsList, userFeedListLoading, userFeedList: userFeedsList = [] } = useFeedsList();
 
     const [firstTime, setFirstTime] = useState(true);
+    const [headerHeight, setHeaderHeight] = useState(0);
+    const [hightMeasured, setHeightMeasured] = useState(false);
+
+    const tabBarHeight = useBottomTabBarHeight();
 
     const currentScreen = useNavigationState((state) => {
         const route = state.routes[state.index];
@@ -69,26 +76,57 @@ const FeedsListTemplate = ({ otherUserId }: { otherUserId: string }) => {
         getFeedsList({ otherUserId: otherUserId ? otherUserId : "" });
     }
 
+    const ListEmptyComponent = () => {
+        const remainingHeight = height - headerHeight - tabBarHeight;
+
+        return (
+            <>
+                <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
+                    <Image source={Images.NoPostsImage} style={{ width, height: remainingHeight, resizeMode: "contain" }} />
+                    <Text style={{
+                        fontSize: 20, fontWeight: "bold", color: "black", position: 'absolute',
+                        bottom: 100, textAlign: 'center'
+                    }}>No Posts Found</Text>
+                </View>
+            </>
+        )
+    }
+
+    const ListHeaderComponent = () => {
+        return (
+            <>
+                <View onLayout={(event) => {
+                    const { height } = event.nativeEvent.layout;
+                    setHeaderHeight(height);
+                    setHeightMeasured(true);
+                }}>
+                    {
+                        isHomePage ?
+                            <View style={styles.imageContainer}>
+                                <Image source={Images.InstagramLogo} style={styles.logoStyle} />
+                            </View> :
+                            <NavigationBar rightProps={{ onBack: () => navigation.goBack(), back: true, right: false, text: "Posts" }} navigation={navigation} />
+                    }
+
+                </View>
+            </>
+        )
+    }
+
     return (
-        <>
-            {
-                isHomePage ?
-                    <View style={styles.imageContainer}>
-                        <Image source={Images.InstagramLogo} style={styles.logoStyle} />
-                    </View> :
-                    <NavigationBar rightProps={{ onBack: () => navigation.goBack(), back: true, right: false, text: "Posts" }} navigation={navigation} />
-            }
-            <View style={{ flex: 1, backgroundColor: "white" }} >
-                <FlatList
-                    data={userFeedsList ? userFeedsList : []}
-                    renderItem={({ item, index }) => renderFeeds(item, index)}
-                    keyExtractor={(_, index) => index.toString()}
-                    onRefresh={onRefresh}
-                    refreshing={userFeedListLoading}
-                />
-                <Loader visible={userFeedListLoading || deleteUserFeedLoading} />
-            </View>
-        </>
+        <View style={{ flex: 1, backgroundColor: "white" }}>
+            <FlatList
+                data={userFeedsList.length > 0 ? userFeedsList : []}
+                renderItem={({ item, index }) => renderFeeds(item, index)}
+                keyExtractor={(_, index) => index.toString()}
+                onRefresh={onRefresh}
+                refreshing={userFeedListLoading}
+                ListEmptyComponent={() => hightMeasured && !(userFeedListLoading || deleteUserFeedLoading) && <ListEmptyComponent />}
+                ListHeaderComponent={() => <ListHeaderComponent />}
+                scrollEnabled={userFeedsList && userFeedsList.length > 0}
+            />
+            <Loader visible={userFeedListLoading || deleteUserFeedLoading} />
+        </View>
     )
 }
 
