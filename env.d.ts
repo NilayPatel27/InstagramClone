62418a29562c2442a81a686dba2a2197b44e1831
NilayPatel27 <nilaykumarkanubhai.patel@ontariotechu.net>
2024-12-18 21:56:41 -0500

    Configure for reading env files in React Native

---
+++ env.d.ts
@@ -0,0 +1,15 @@
+declare module '@env' {
+    export const LOGIN: string;
+    export const SIGNUP: string;
+    export const USERNAMEEXIST: string;
+    export const USEREMAILEXIST: string;
+    export const FEEDUPLOAD: string;
+    export const FEEDLIST: string;
+    export const DELETEACCOUNT: string;
+    export const DELETEPOST: string;
+    export const ALLUSERSLIST: string;
+    export const USERDETAILS: string;
+    export const UPDATEPROFILE: string;
+    export const FOLLOWUSER: string;
+    export const UNFOLLOWUSER: string;
+}
