5f240f9b6ed1c62ab3b55aed7c6e029ecbd76910
NilayPatel27 <nilaykumarkanubhai.patel@ontariotechu.net>
2024-12-25 20:37:30 -0500

    Test gitignore file change

--- server-2/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server-2/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f78b5b6d81316a2af92918f252db195aecc5461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06de1e69b6fa059c5082496207a6cf0e9375f3a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
